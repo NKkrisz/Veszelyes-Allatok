22a85c12a4fa83dcf3156d0fb8706460bd53a247
varma01 <varma01@mail.com>
2020-12-09 20:42:42 +0100

    Update 0.4

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 function switchTo(num){
     menuBtns = document.getElementsByClassName("menu-btn");
     pages = document.getElementsByClassName("content");
+    
     for (let btn of menuBtns){
-        btn.setAttribute("class", "menu-btn");}
+        btn.setAttribute("class", "menu-btn");
+    }
     menuBtns[num].setAttribute("class", "menu-btn menu-selected");
+
     for (let page of pages){
-        page.hidden = true;}
+        page.hidden = true;
+    }
     pages[num].hidden = false;
+};
+
+function toggleMenu(){
+    menu = document.getElementsByClassName("menu")[0];
+    if (menu.style.visibility == "visible"){
+        menu.style.visibility = "hidden";
+    }else{
+        menu.style.visibility = "visible";
+    }
 };
 //haha javascript go brrr
