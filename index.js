e4d7c75d8958bc6533eb079335e5cce90e47fff6
varma01 <varma01@mail.com>
2020-12-09 18:13:12 +0100

    Update 0.2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function switchTo(num){
+    menuBtns = document.getElementsByClassName("menu-btn");
+    pages = document.getElementsByClassName("content");
+    for (let btn of menuBtns){
+        btn.setAttribute("class", "menu-btn");}
+    menuBtns[num].setAttribute("class", "menu-btn menu-selected");
+    for (let page of pages){
+        page.hidden = true;}
+    pages[num].hidden = false;
+};
+//haha javascript go brrr
