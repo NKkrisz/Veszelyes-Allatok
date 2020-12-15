bf3918fc4c90149fe148fb6868ba43ac5234767f
varma01 <varma01@mail.com>
2020-12-15 09:25:11 +0100

    Update 0.1BROKEN

--- webserver.py
+++ webserver.py
@@ -1,9 +1,13 @@
+import json
 from flask import *
 app = Flask(__name__,template_folder="./",static_folder="./",static_url_path="")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    with open("pages.json","r") as f:
+        pages = json.loads(f.read())
+        f.close()
+    return render_template("index.html", pages=pages)
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8493, debug=True)
